b0fb743c2db06c8fb1576e27d71ac9400c48a6e2
TitarS <titar.serge@gmail.com>
2018-07-28 18:22:59 +0300

    change Categories model

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
+    Route::resource('/category', 'CategoryController', ['except' => 'show']);
+    Route::get('/category/sort', 'CategoryController@sort')->name('sort');
+    Route::post('/category/categorySortAjax', 'CategoryController@sortAjax')->name('categorySortAjax');
+});
+
+Route::get('/category', 'CategoryController@index');
